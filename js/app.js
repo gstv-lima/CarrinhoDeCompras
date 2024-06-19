e458b64c7a769b1bb57e1a666faffa49bfd83147
unknown <gustavodelima1900@gmail.com>
2024-06-19 10:58:20 -0300

    CarrinhoDeCompras

---
+++ js/app.js
@@ -0,0 +1,27 @@
+let totalGeralCarrinho = 0;
+
+function adicionar(){
+    //Recupera valores - nome do produto - quantidade e valor;
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valUnidade = produto.split('R$')[1];
+    let qntd = document.getElementById('quantidade').value;
+    
+    //Calcula o preço e mostra produto no carrinho;
+    let preço = qntd * valUnidade;
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${qntd}x</span> ${nomeProduto} <span class="texto-azul">R$${preço}</span>
+        </section>`;
+
+    //Calcula valor total dos produtos no carrinho;
+    totalGeralCarrinho = totalGeralCarrinho + preço;
+    
+    valorTotal = document.getElementById('valor-total');
+    valorTotal.innerText = `R$${totalGeralCarrinho}`;
+
+}
+
+function limpar(){
+
+}
